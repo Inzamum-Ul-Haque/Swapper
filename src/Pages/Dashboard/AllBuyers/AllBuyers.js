b2a796fee3bb11ad8b5754850743d372f3293ae4
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-27 02:16:54 +0600

    admin delete user done

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Loading from "../../Shared/Loading/Loading";
 import { MdDelete } from "react-icons/md";
 import { TiTick } from "react-icons/ti";
+import { toast } from "react-hot-toast";
 
 const AllBuyers = () => {
-  const { data: allBuyers = [], isLoading } = useQuery({
+  const [loadingButton, setLoadingButton] = useState(false);
+  const {
+    data: allBuyers = [],
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["buyers"],
     queryFn: async () => {
       const res = await axios.get(
@@ -20,6 +26,24 @@ const AllBuyers = () => {
   if (isLoading) {
     return <Loading />;
   }
+
+  const handleUserDelete = (email) => {
+    setLoadingButton(true);
+    fetch(`http://localhost:5000/user?email=${email}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status) {
+          toast.success(result.message);
+          setLoadingButton(false);
+          refetch();
+        } else {
+          toast.error(result.message);
+          setLoadingButton(false);
+        }
+      });
+  };
 
   return (
     <div>
@@ -60,9 +84,41 @@ const AllBuyers = () => {
                     )}
                   </td>
                   <td>
-                    <button className="text-white text-sm btn btn-sm bg-red-500 hover:bg-red-600 hover:border-red-600">
-                      Delete <MdDelete className="ml-1" />
-                    </button>
+                    {loadingButton ? (
+                      <button
+                        onClick={() => handleUserDelete(singleUser.email)}
+                        className="text-white text-sm btn btn-sm bg-red-500 hover:bg-red-600 hover:border-red-600"
+                      >
+                        Delete{" "}
+                        <svg
+                          className="w-3 h-3 mr-3 -ml-1 text-white animate-spin"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => handleUserDelete(singleUser.email)}
+                        className="text-white text-sm btn btn-sm bg-red-500 hover:bg-red-600 hover:border-red-600"
+                      >
+                        Delete <MdDelete className="ml-1" />
+                      </button>
+                    )}
                   </td>
                 </tr>
               ))}
