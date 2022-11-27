c4e8efc73f7a7628655be18c37cd50236680248a
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-11-27 12:21:45 +0600

    products shown in my products

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AiFillSound, AiOutlineInfoCircle } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { AuthContext } from "../../../Contexts/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Loading from "../../Shared/Loading/Loading";
 
 const MyProducts = () => {
-  return <div>my products</div>;
+  const { user } = useContext(AuthContext);
+
+  const { data: allProducts, isLoading } = useQuery({
+    queryKey: ["products", user?.email],
+    queryFn: async () => {
+      const res = await axios.get(
+        `http://localhost:5000/products?email=${user?.email}`
+      );
+      return res.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  const { data } = allProducts;
+
+  return (
+    <div>
+      {data.length === 0 ? (
+        <h1 className="text-4xl text-center mt-16 text-gray-600">
+          No products to show
+        </h1>
+      ) : (
+        <>
+          <h1 className="text-4xl text-secondary text-left mt-8 ml-5 mb-5">
+            My Products
+          </h1>
+          <div className="mt-5 ml-5">
+            {data.map((product) => (
+              <div
+                key={product._id}
+                className="flex flex-col mb-5 w-full md:flex-row border rounded-lg bg-white shadow-lg"
+              >
+                <img
+                  className="lg:w-64 md:w-64 sm:w-full object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+                  src={product.productImage}
+                  alt=""
+                />
+                <div className="p-6 flex flex-col justify-start text-left">
+                  <h5 className="text-gray-900 text-xl font-medium mb-2">
+                    {product.productName}
+                  </h5>
+                  <div className="flex mb-2">
+                    <p className="text-lg mr-10">
+                      Original Price: Tk {product.productOriginalPrice}
+                    </p>
+                    <p className="text-lg mr-10">
+                      Regular Price: Tk {product.productResalePrice}
+                    </p>
+                    <p className="text-lg">
+                      Category: <b>{product.productCategory}</b>
+                    </p>
+                  </div>
+                  <p className="text-gray-700 text-base mb-4">
+                    {product.productDescription.length < 200
+                      ? product.productDescription
+                      : product.productDescription.slice(0, 300) + " ..."}
+                  </p>
+                  <div className="flex lg:flex-row md:flex-row sm:flex-col lg:justify-end md:justify-center items-center">
+                    <button className="flex items-center btn w-max bg-green-500 text-sm text-white hover:bg-green-700 hover:border-green-700 mb-4">
+                      Advertise <AiFillSound className="ml-2" />
+                    </button>
+                    <button className="flex items-center btn w-max bg-red-500 text-sm text-white hover:bg-red-700 hover:border-red-700 mb-4 ml-4">
+                      Delete this product <MdDelete className="ml-2" />
+                    </button>
+                    <button className="flex items-center btn w-max bg-primary text-sm text-white hover:bg-primary hover:border-primary mb-4 ml-4">
+                      View Details <AiOutlineInfoCircle className="ml-2" />
+                    </button>
+                  </div>
+                  <p className="text-gray-600 text-sm">Posted on: </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MyProducts;
